69398025b30942f5ac286ee0bd8564a72b29a72e
jansou <jansou74@gmail.com>
2015-10-05 16:00:55 +0900

    ダメージアニメーションの追加

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -155,6 +155,10 @@ public class Player : MonoBehaviour
                 //プレイヤー削除
                 Destroy(gameObject);
             }
+            else
+            {
+                spaceship.GetAnimator().SetTrigger("Damage");
+            }
 		}
 
 	}
