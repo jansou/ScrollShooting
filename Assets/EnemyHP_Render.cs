f6f205d5e860e7ca2d7d50f790d9b756767e67b5
jansou <jansou74@gmail.com>
2015-10-19 18:29:46 +0900

    敵に体力ゲージを付けた。 ※体力ゲージを基準に弾が発射されている

---
+++ Assets/EnemyHP_Render.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHP_Render : MonoBehaviour 
+{
+    Enemy enemy;
+
+    float maxhp=0;
+    float hp = 0;
+
+	// Use this for initialization
+	void Start () 
+    {
+        enemy = transform.parent.parent.GetComponent<Enemy>();
+
+        maxhp = enemy.hp;
+        hp = maxhp;
+        if(maxhp<=0)
+        {
+            Debug.Log("不正な体力値です");
+        }
+
+        
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        hp = enemy.hp;
+
+        transform.localScale = new Vector3(hp / maxhp,1,1);
+	}
+}
