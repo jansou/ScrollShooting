4cede135f481c34c7b7182a9a1caee9ca4b94f77
hisoji <isoji.gsynd10me@gmail.com>
2015-10-19 17:09:56 +0900

    FormationButton.cs追加

---
+++ Assets/Scripts/FormationButton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FormationButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Button button = GetComponent<Button>();
+		button.onClick.AddListener(()=>{
+			FindObjectOfType<Party>().ChangeFormation();
+		});
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
