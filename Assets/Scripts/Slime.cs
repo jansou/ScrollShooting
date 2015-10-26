b46300d42416b05ffa37242d0b8031caa9706cf0
jansou <jansou74@gmail.com>
2015-10-26 15:13:56 +0900

    no message

--- Assets/Scripts/Slime.cs
+++ Assets/Scripts/Slime.cs
@@ -4,20 +4,27 @@ using System.Collections;
 public class Slime : MonoBehaviour {
 	Spaceship spaceship;
 
+	Transform shotPositions;
 	// Use this for initialization
 	IEnumerator Start () {
 		spaceship = GetComponent<Spaceship> ();
 
+		GameObject oo = new GameObject();
+		oo.name = "shotPositions";
+		oo.transform.parent = transform;
+		oo.transform.localPosition = new Vector3(0,0,0);
+		shotPositions = oo.transform;
+
 		GameObject o = (GameObject)Instantiate(Resources.Load("ShotPosition"));
-		o.transform.parent = transform;
+		o.transform.parent = shotPositions;
 		o.transform.localPosition = new Vector3(0,0,0);
 
 		while (true) 
 		{
 			//子要素を全て取得する
-			for(int i=0;i<transform.childCount;++i)
+			for(int i=0;i<shotPositions.childCount;++i)
 			{
-				Transform shotPosition = transform.GetChild(i);
+				Transform shotPosition = shotPositions.GetChild(i);
 
 				shotPosition.localRotation = Quaternion.Euler(0,0,60+Random.Range(0,60)); 
 				
