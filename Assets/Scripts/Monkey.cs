bb2bf5a690f489b2c8ed7801d4c6260af081b0db
hisoji <isoji.gsynd10me@gmail.com>
2015-10-26 14:41:11 +0900

    HPゲージが傾かないようにした

--- Assets/Scripts/Monkey.cs
+++ Assets/Scripts/Monkey.cs
@@ -4,23 +4,30 @@ using System.Collections;
 public class Monkey : MonoBehaviour {
 
 	Spaceship spaceship;
-	
+	Transform shotPositions;
+
 	// Use this for initialization
 	IEnumerator Start () {
 		spaceship = GetComponent<Spaceship> ();
 
+		GameObject oo = new GameObject();
+		oo.name = "shotPositions";
+		oo.transform.parent = transform;
+		oo.transform.localPosition = new Vector3(0,0,0);
+		shotPositions = oo.transform;
+
 		for(int i=0; i<2; ++i){
 			GameObject o = (GameObject)Instantiate(Resources.Load("ShotPosition"));
-			o.transform.parent = transform;
+			o.transform.parent = shotPositions;
 			o.transform.localPosition = new Vector3(0,0,0);
 		}
 		
 		while (true) 
 		{
 			//子要素を全て取得する
-			for(int i=0;i<transform.childCount;++i)
+			for(int i=0;i<shotPositions.childCount;++i)
 			{
-				Transform shotPosition = transform.GetChild(i);
+				Transform shotPosition = shotPositions.GetChild(i);
 				
 				shotPosition.localRotation = Quaternion.Euler(0,0,60+Random.Range(0,60)); 
 				
