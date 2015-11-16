499bf7d706af82017c8d4705a14f9bf69922bc54
hisoji <isoji.gsynd10me@gmail.com>
2015-11-16 16:34:51 +0900

    移動範囲の調整

--- Assets/Scripts/Party.cs
+++ Assets/Scripts/Party.cs
@@ -114,7 +114,7 @@ public class Party : MonoBehaviour {
 
        
 
-		Vector2 min = scale * -0.5f;
+		Vector2 min = new Vector2(scale.x * -0.5f,scale.y*-0.2f);
         Vector2 max = scale * 0.5f;
 
 		min.x += 1;
