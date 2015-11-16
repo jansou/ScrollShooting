c32e7f74d01d8c94375228bca773f364f60662b3
jansou <jansou74@gmail.com>
2015-11-16 17:09:53 +0900

    HPゲージ作りかけ

---
+++ Assets/Scripts/PlayerHP_Render.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHP_Render : MonoBehaviour 
+{
+    //Player player;
+
+    float maxhp=0;
+    float hp = 0;
+
+	// Use this for initialization
+	void Start () 
+    {
+
+        //player = transform.parent.parent.GetComponent<Player>();
+
+        //maxhp = player.hp;
+        //hp = maxhp;
+        //if(maxhp<=0)
+        //{
+         //   Debug.Log("不正な体力値です");
+        //}
+
+        
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        //hp = player.hp;
+
+        //transform.localScale = new Vector3(hp / maxhp,1,1);
+	}
+}
