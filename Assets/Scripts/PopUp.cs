8b226fc0d6f446a37d500b6c597b5c2dee18e7ae
hisoji <isoji.gsynd10me@gmail.com>
2015-12-19 16:42:45 +0900

    敵に当たると数字が出るように(見にくい) 敵の色を少し直した

---
+++ Assets/Scripts/PopUp.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PopUp : MonoBehaviour {
+	public Canvas drawCanvas;
+	public GameObject textObj;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void CreateText(Vector3 position,int num){
+		GameObject o = (GameObject)Instantiate(textObj);
+		o.transform.position = Camera.main.WorldToScreenPoint(position);
+		o.transform.SetParent(drawCanvas.transform);
+		
+		o.GetComponent<Text>().text = num.ToString();
+	}
+}
