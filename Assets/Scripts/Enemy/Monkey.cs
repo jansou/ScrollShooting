346e4e811dd7938b6185ff241f563c6f33d6bb80
jansou <jansou74@gmail.com>
2016-01-21 17:36:00 +0900

    森の背景上部を追加。Monkeyの弾を自機狙いに変更

--- Assets/Scripts/Enemy/Monkey.cs
+++ Assets/Scripts/Enemy/Monkey.cs
@@ -5,6 +5,10 @@ public class Monkey : MonoBehaviour {
 	Spaceship spaceship;
 	EnemyCommon common;
     public int power = 2;
+
+    Transform s1;
+    Transform pt;
+
     //SE関係
     public AudioClip shootSE;
     AudioSource shootSESource;
@@ -17,12 +21,15 @@ public class Monkey : MonoBehaviour {
         shootSESource = gameObject.GetComponent<AudioSource>();
         shootSESource.clip = shootSE;
 
-		Transform s1 = common.CreateShotPosition();
+        s1 = common.CreateShotPosition();
+        pt = FindObjectOfType<Party>().transform;
 
 		yield return new WaitForEndOfFrame();
 		while (true) 
 		{
-			common.Shot(s1,60+Random.Range(0,60),power,2,BulletManager.BulletType.BananaSlash);
+			//common.Shot(s1,60+Random.Range(0,60),power,2,BulletManager.BulletType.BananaSlash);
+            common.ShotAim(s1, pt, power, 3, BulletManager.BulletType.BananaSlash);
+                
             shootSESource.PlayOneShot(shootSE);
 
 			yield return new WaitForSeconds(spaceship.shotDelay);
