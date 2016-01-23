62b47fbd0faa0ce5df903d65414f27573097a46f
jansou <jansou74@gmail.com>
2016-01-23 19:35:13 +0900

    PlayCommon.prefabに共通の必要なものをまとめた。

--- Assets/Prefabs/BGMManager.cs
+++ Assets/Prefabs/BGMManager.cs
@@ -4,15 +4,29 @@ using System.Collections;
 public class BGMManager : MonoBehaviour {
 	public AudioClip stageBGM;
 	public AudioClip bossBGM;
+    AudioSource audioSource;
+
+    void Awake()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+    }
+
 	// Use this for initialization
 	void Start () {
+        //SE関係
+        //audioSource = gameObject.GetComponent<AudioSource>();
+        //
 	}
 
 	public void SetBossBGM(){
-		GetComponent<AudioSource>().clip = bossBGM;
+		//GetComponent<AudioSource>().clip = bossBGM;
+        audioSource.clip = bossBGM;
+        audioSource.Play();
 	}
 	public void SetStageBGM(){
-		GetComponent<AudioSource>().clip = stageBGM;
+		//GetComponent<AudioSource>().clip = stageBGM;
+        audioSource.clip = stageBGM;
+        audioSource.Play();
 	}
 	// Update is called once per frame
 	void Update () {
