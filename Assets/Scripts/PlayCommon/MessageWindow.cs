fb4bc9cdf94b4366703cf29c4877a6689004a88b
hisoji <isoji.gsynd10me@gmail.com>
2016-01-25 17:39:35 +0900

    クリアとコンティニュー文字差し替え

--- Assets/Scripts/PlayCommon/MessageWindow.cs
+++ Assets/Scripts/PlayCommon/MessageWindow.cs
@@ -6,7 +6,7 @@ public class MessageWindow : MonoBehaviour {
 	Text text;
 	Canvas canvas;
 
-	int displayFrame = 0;
+	float displayFrame = 0;
 	// Use this for initialization
 	void Start () {
 		canvas = GetComponent<Canvas>();
@@ -17,8 +17,8 @@ public class MessageWindow : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if(displayFrame > 0){
-			--displayFrame;
-			if(displayFrame == 0){
+			displayFrame -= Time.deltaTime;
+			if(displayFrame < 0){
 				canvas.enabled = false;
 			}
 		}
@@ -28,7 +28,7 @@ public class MessageWindow : MonoBehaviour {
 	public void showMessage(string message){
 		text.text = message;
 		canvas.enabled = true;
-		displayFrame = 120;
+		displayFrame = 2;
 	}
 
 }
