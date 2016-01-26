77af15c3ddcfa400e6d0fd71d56c173a98614c71
jansou <jansou74@gmail.com>
2016-01-27 01:12:02 +0900

    敵の経験値の適用

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -200,81 +200,8 @@ public class Player : MonoBehaviour
 
 	void Update ()
 	{
-        /*
-		// 右・左
-		//float x = Input.GetAxisRaw ("Horizontal");
-        float x = CrossPlatformInputManager.GetAxisRaw("Horizontal");
 
-		// 上・下
-		//float y = Input.GetAxisRaw ("Vertical");
-        float y = CrossPlatformInputManager.GetAxisRaw("Vertical");
-
-		// 移動する向きを求める
-		//Vector2 direction = new Vector2 (x, y).normalized;
-        Vector2 direction = new Vector2(x, y);
-		Move (direction);
-
-		// 移動する向きとスピードを代入する
-		//GetComponent<Rigidbody2D>().velocity = direction * speed;
-		//spaceship.Move (direction);
-
-		//移動の制限(だめな例)
-		//Clamp ();
-         */
 	}
-
-    /*
-	void Move(Vector3 direction)
-	{
-        //背景のスケール
-        Vector2 scale = background.transform.localScale;
-        
-		//画面左下のワールド座標をビューポートから取得
-		//Vector2 min = Camera.main.ViewportToWorldPoint (new Vector2 (0, 0));
-
-		//画面右上のワールド座標をビューポートから取得
-		//Vector2 max = Camera.main.ViewportToWorldPoint (new Vector2 (1, 1));
-        
-
-        Vector2 min = scale * -0.5f;
-
-        Vector2 max = scale * 0.5f;
-		//プレイヤー座標の取得
-		Vector3 pos = transform.position;
-
-		//移動量を加える
-		pos += direction * spaceship.speed * Time.deltaTime;
-
-		//プレイヤーの位置が画面内に収まるように制限をかける
-		pos.x = Mathf.Clamp (pos.x, min.x, max.x);
-		pos.y = Mathf.Clamp (pos.y, min.y, max.y);
-
-		//制限をかけた値をプレイヤーの位置とする
-		transform.position = pos;
-	}
-    */
-
-	/*//画面外に一瞬はみ出してしまうため、よくない
-	void Clamp()
-	{
-		//画面左下のワールド座標をビューポートから取得
-		Vector2 min = Camera.main.ViewportToWorldPoint (new Vector2 (0, 0));
-
-		//画面右上のワールド座標をビューポートから取得
-		Vector2 max = Camera.main.ViewportToWorldPoint (new Vector2 (1, 1));
-
-		//プレイヤーの座標を取得
-		Vector2 pos = transform.position;
-
-		//プレイヤーの位置が画面内になるように制限をかける
-		pos.x = Mathf.Clamp (pos.x, min.x, max.x);
-		pos.y = Mathf.Clamp (pos.y, min.y, max.y);
-
-		//制限をかけた値をプレイヤーの位置とする
-		transform.position = pos;
-	}
-	*/
-
 
 	// ぶつかった瞬間に呼び出される
 	void OnTriggerEnter2D (Collider2D c)
@@ -361,7 +288,8 @@ public class Player : MonoBehaviour
 		
 	}
 
-	int nextLevel(){
+	int nextLevel()
+    {
 		return needExpByLevel(level);
 	}
 	int preLevel(){
@@ -371,23 +299,38 @@ public class Player : MonoBehaviour
 //HP,Power by Character
 //キャラクター毎の調整を以下で行う(HP,攻撃力,必要経験値)
 
-	int HPByLevel(int level){
-		switch(type){
+	int HPByLevel(int level)
+    {
+		switch(type)
+        {
+        case Type.Alex:
+            return 12 + 3 * level;
 		case Type.Guylus:
-			return 15*level;
+			return 15 + 4*level;
+        case Type.Nely:
+            return 8 + 2 * level;
 		default:
 			return 10 * level;
 		}
 	}
-	int PowerByLevel(int level){
+	int PowerByLevel(int level)
+    {
+        //Alex,
+		//Guylus,
+		//Nely,
+
 		switch(type){
-		case Type.Alex:
-			return 1 + (level/2);
-		default:
-			return 1 + (level/4);
+            case Type.Alex:
+                return 1 + (level / 5);
+            case Type.Guylus:
+                return 1 + (level / 6);
+            case Type.Nely:
+                return 1 + (level / 4);
+		    default:
+			    return 1 + (level/4);
 		}
 	}
 	int needExpByLevel(int level){
-		return 100 * level * level;
+		return 200 * level;
 	}
 }
