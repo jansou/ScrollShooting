937b86a5db2a96ac14f0a2c088b113a106eec5b1
jansou <jansou74@gmail.com>
2016-02-25 18:57:12 +0900

    敵のプレハブデータフォルダを整理しました。敵の情報を更新して、ステージ概要2.pdfとしました

---
+++ Assets/Scripts/Enemy/BossGiftman.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossGiftman : MonoBehaviour {
+	Spaceship spaceship;
+	EnemyCommon common;
+    Enemy enemy;
+
+    public int power=2;
+
+	Transform s2;
+	Transform pt;
+
+    //SE関係
+    public AudioClip shootSE;
+    public AudioClip skillSE;
+    AudioSource audioSource;
+
+	// Use this for initialization
+	IEnumerator Start () {
+		spaceship = GetComponent<Spaceship> ();
+		common = GetComponent<EnemyCommon>();
+        enemy = GetComponent<Enemy>();
+		common.Init();
+
+        //SE関係
+        audioSource = gameObject.GetComponent<AudioSource>();
+        audioSource.clip = shootSE;
+        //
+
+		s2 = common.CreateShotPosition();
+		pt = FindObjectOfType<Party>().transform;
+
+		FindObjectOfType<MessageWindow>().showMessage("メテオ");
+
+		yield return new WaitForEndOfFrame();
+
+        StartCoroutine("Stop");
+		StartCoroutine("Attack1");
+		//StartCoroutine("Attack2");
+
+		yield break;
+	}
+
+    IEnumerator Stop()
+    {
+        FindObjectOfType<MessageWindow>().showMessage("ボスザル");
+        yield return new WaitForSeconds(2);
+        enemy.MoveStop();
+    }
+
+	IEnumerator Attack1(){//banana
+		while (true) 
+		{
+            for (int n=0; n<10;++n )
+            {
+                audioSource.PlayOneShot(shootSE);
+                common.ShotAim(s2, pt, power, 3, BulletManager.BulletType.BananaSlash);
+                
+                //shotDelay秒待つ
+                yield return new WaitForSeconds(spaceship.shotDelay);
+            }
+
+            //audioSource.clip = skillSE;
+            audioSource.PlayOneShot(skillSE);
+            FindObjectOfType<MessageWindow>().showMessage("バナナラッシュ！");
+
+			spaceship.GetAnimator().SetTrigger("Skill");
+			yield return new WaitForSeconds(0.5f);
+
+            //バナナラッシュ
+            for (int m = 0; m < 2; ++m)
+            {
+                for (int n = 0; n < 4; ++n)
+                {
+                    for (int i = 0; i < 6; ++i)
+                    {
+                        audioSource.PlayOneShot(shootSE);
+                        common.Shot(s2, -45+ 75*m + 30 * i, power, 4 - n, BulletManager.BulletType.BananaSlash);
+                    }
+
+                }
+                yield return new WaitForSeconds(spaceship.shotDelay + 1.0f);
+            }
+
+			//shotDelay秒待つ
+			yield return new WaitForSeconds(spaceship.shotDelay + 2.5f);
+		}
+	}
+    /*
+	IEnumerator Attack2(){//3way
+		while (true) 
+		{
+			for(int i=0; i<3; ++i){
+				common.Shot(s2, 60+30*i,1,2);
+			}
+			
+			//shotDelay秒待つ
+			yield return new WaitForSeconds(spaceship.shotDelay);
+		}
+	}
+	*/
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
